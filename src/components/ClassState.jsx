9e9fe70b59880df0ca2aad1cb9cdfa06b3e6c35c
Héctor Airy <hola@hectorairy.com>
2022-10-08 11:14:23 -0500

    feat: simple states, React.Component vs React Hooks

--- src/components/ClassState.jsx
+++ src/components/ClassState.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 
 export class ClassState extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      error: false,
+    };
+  }
+
   render() {
     return (
       <div>
         <h1>Eliminar {this.props.name}</h1>
+        <p>Por favor, escribe el código de seguridad.</p>
+
+        {this.state.error && <p>Error: el código es incorrecto</p>}
+
+        <input type="text" placeholder="Código de seguridad" />
+        <button onClick={() => this.setState({ error: !this.state.error })}>
+          Comprobar
+        </button>
       </div>
     );
   }
--- src/components/UseState.jsx
+++ src/components/UseState.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 
 export const UseState = ({ name }) => {
+  const [error, setError] = React.useState(false);
+
   return (
     <div>
       <h1>Eliminar {name}</h1>
+      <p>Por favor, escribe el código de seguridad.</p>
+
+      {error && <p>Error: el código es incorrecto</p>}
+
+      <input type="text" placeholder="Código de seguridad" />
+      <button onClick={() => setError(!error)}>Comprobar</button>
     </div>
   );
 };

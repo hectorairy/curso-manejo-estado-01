b0d1e86ca498263cd6292d1b7b642cdecb869950
Héctor Airy <hola@hectorairy.com>
2022-10-08 11:21:39 -0500

    feat: create loading state, and use React.useEffect to change it

--- src/components/UseState.jsx
+++ src/components/UseState.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 export const UseState = ({ name }) => {
-  const [error, setError] = React.useState(false);
+  const [error] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    console.log("Empezando el useEffect");
+    if (loading) {
+      setTimeout(() => {
+        console.log("Empezando cambio del estado loading");
+        setLoading(false);
+        console.log("Terminando cambio del estado loading");
+      }, 3000);
+    }
+    console.log("Terminando el useEffect");
+  }, [loading]);
 
   return (
     <div>
@@ -10,8 +23,10 @@ export const UseState = ({ name }) => {
 
       {error && <p>Error: el código es incorrecto</p>}
 
+      {loading && <p>Cargando...</p>}
+
       <input type="text" placeholder="Código de seguridad" />
-      <button onClick={() => setError(!error)}>Comprobar</button>
+      <button onClick={() => setLoading(true)}>Comprobar</button>
     </div>
   );
 };

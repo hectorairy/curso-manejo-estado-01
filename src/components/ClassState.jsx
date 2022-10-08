18f1da200fdb59001606a95500c5c628a3b1a2b2
Héctor Airy <hola@hectorairy.com>
2022-10-08 11:31:24 -0500

    feat: create loading state with React.Component, and use lifecycle to change it

--- src/components/ClassState.jsx
+++ src/components/ClassState.jsx
@@ -1,11 +1,32 @@
 import React from "react";
+import { Loading } from "./Loading";
 
 export class ClassState extends React.Component {
   constructor() {
     super();
     this.state = {
       error: false,
+      loading: false,
     };
+  }
+
+  //   componentDidMount() {
+  //     console.log("componentDidMount");
+  //   }
+
+  //   UNSAFE_componentWillMount() {
+  //     console.log("componentWillMount");
+  //   }
+
+  componentDidUpdate() {
+    console.log("Actualizamos el componente ClassState");
+    if (this.state.loading) {
+      setTimeout(() => {
+        console.log("Empezando cambio del estado loading");
+        this.setState({ loading: false });
+        console.log("Terminando cambio del estado loading");
+      }, 3000);
+    }
   }
 
   render() {
@@ -16,8 +37,10 @@ export class ClassState extends React.Component {
 
         {this.state.error && <p>Error: el código es incorrecto</p>}
 
+        {this.state.loading && <Loading />}
+
         <input type="text" placeholder="Código de seguridad" />
-        <button onClick={() => this.setState({ error: !this.state.error })}>
+        <button onClick={() => this.setState({ loading: true })}>
           Comprobar
         </button>
       </div>
